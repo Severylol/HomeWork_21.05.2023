9914025d75c7393f3599d4eafcaaf97961f47d56
Louisa <131443655+Severylol@users.noreply.github.com>
2023-05-26 15:49:19 +0300

    first commit

---
+++ task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int secondDigit = (number / 10) % 10;
+Console.WriteLine("Вторая цифра числа: " + secondDigit);
