7900aa8b13c5b0327f6212686cd5c234823106fa
Louisa <131443655+Severylol@users.noreply.github.com>
2023-05-26 16:14:59 +0300

    two commit

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+    int number = int.Parse(Console.ReadLine()!);
+    int digitCount = 0;
+    int tempNumber = number;
+while (tempNumber > 0)
+{
+    digitCount++;
+    tempNumber /= 10;
+}
+if (digitCount >= 3)
+{
+    int thirdDigit = (number / (int)Math.Pow(10, digitCount - 3)) % 10;
+Console.WriteLine("Третья цифра: " + thirdDigit);
+}
+else
+{
+Console.WriteLine("Третьей цифры нет.");
+}
---
+++ task15/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблиись, в неделе 7 дней!\nВведите номер дня недели: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if (n == 1)
+    Console.WriteLine("Будний день - Понедельник");
+else if (n == 2)
+    Console.WriteLine("Будний день - Вторник");
+else if (n == 3)
+    Console.WriteLine("Будний день - Среда");
+else if (n == 4)
+    Console.WriteLine("Будний день - Четверг");
+else if (n == 5)
+    Console.WriteLine("Будний день - Пятница");
+else if (n == 6)
+    Console.WriteLine("Сегодня выходной - Суббота");
+else
+    Console.WriteLine("Сегодня выходной - Воскресенье");
